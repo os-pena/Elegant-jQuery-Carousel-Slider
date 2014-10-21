15c87b2e8a375e9d4d7adeaaf65a9d8b4d59ee59
os-pena <os.a.pena@att.net>
2014-10-21 01:18:25 -0500

    Initial commit

---
+++ js/elegant_slider.js
@@ -0,0 +1,61 @@
+﻿/*---------------------------------------------------------------------------------------------
+
+@author       Oscar Peña - @os-pena
+@link            ???
+@github        https://github.com/os-pena/elegant-jQuery-Carousel-Slider
+@version     0.0.2
+@license      ISC license
+based on Simple-jQuery-Carousel-Slider : https://github.com/paulmason/Simple-jQuery-Carousel-Slider
+----------------------------------------------------------------------------------------------*/
+
+
+jQuery(function ($) {
+
+    // settings
+    var $slider = $('.slider'); // class or id of carousel slider
+    var $slide = 'li'; // could also use 'img' if you're not using a li
+    var $transition_time = 1000; // 1 second
+    var $time_between_slides = 2300; // 2.3 seconds
+    var $interval;
+
+    function slides() {
+        return $slider.find($slide);
+    }
+
+    slides().fadeOut();
+
+    // set active classes
+    slides().first().addClass('active');
+    slides().first().fadeIn($transition_time);
+
+    // auto scroll 
+    function startloop() {
+        $interval = setInterval(
+
+        function () {
+            var $i = $slider.find($slide + '.active').index();
+
+            slides().eq($i).removeClass('active');
+            slides().eq($i).fadeOut($transition_time);
+
+            if (slides().length == $i + 1) $i = -1; // loop to start
+
+            slides().eq($i + 1).fadeIn($transition_time);
+            slides().eq($i + 1).addClass('active');
+        }, $transition_time + $time_between_slides);
+    }
+    function pauseLoop() {
+        window.clearInterval($interval);
+    }
+
+    $(".slider").hover(
+
+    function () {
+        pauseLoop(); // pause the loop
+    },
+
+    function () {
+        startloop(); //scroll()
+    });
+    return startloop();
+});
